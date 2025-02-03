fbf4443e9b91d167a58eb5f233d87a49c1e64838
Haks-DE-Worker <akoredeagn@gmail.com>
2025-02-03 19:55:42 +0100

    Commit for exercise 3 on OOP

--- lib/exercises_on_oop/oop_exercises.dart
+++ lib/exercises_on_oop/oop_exercises.dart
@@ -3,6 +3,13 @@ import 'dart:io';
 // ignore: unused_import
 import 'dart:math';
 
+void main() {
+  // Instance creation
+  Person person = Person('Alice', 30);
+  person.displayInfo(); // Display: Name: Alice, Age: 30
+}
+
+// Class Person definition
 class Person {
   // Class properties
   String name;
@@ -17,8 +24,51 @@ class Person {
   }
 }
 
-void main() {
-  // Instance creation
-  Person person = Person('Alice', 30);
-  person.displayInfo(); // Display: Name: Alice, Age: 30
+class Students extends Person {
+  // class properties
+  int note;
+
+  // Constructor
+  Students(String name, int age, this.note) : super(name, age);
+
+  // A class method
+  void dispalyNote() {
+    print('Name: $name, Age: $age, Note: $note');
+  }
+}
+
+abstract class Forms {
+  // Abstract method
+  void airDeternine();
+}
+
+// class Circle and Rectangle who implements the abstract class Forms
+class Circle implements Forms {
+  // class properties
+  double radius;
+
+  // Constructor
+  Circle(this.radius);
+
+  // Implementation of the abstract method
+  @override
+  void airDeternine() {
+    print('Area of the circle: ${3.14 * radius * radius}');
+  }
+}
+
+// class Rectangle
+class Rectangle implements Forms {
+  // class properties
+  double length;
+  double width;
+
+  // Constructor
+  Rectangle(this.length, this.width);
+
+  // Implementation of the abstract method
+  @override
+  void airDeternine() {
+    print('Area of the rectangle: ${length * width}');
+  }
 }
