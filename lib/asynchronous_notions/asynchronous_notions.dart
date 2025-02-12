c70d1b1e5117d9e7eb4bff39b46ffc810dddcabc
Haks-DE-Worker <akoredeagn@gmail.com>
2025-02-12 21:50:23 +0100

    Commit after creation of method which simulate a sum operation with try-catch clause in order to manage possible errors

--- lib/asynchronous_notions/asynchronous_notions.dart
+++ lib/asynchronous_notions/asynchronous_notions.dart
@@ -1,13 +1,26 @@
 import 'dart:async';
-import 'dart:io';
 
 void main() async {
-  print('Getting data ...');
-  String data = await asyncFunction();
-  print('Data: $data');
+  // print('Getting data ...');
+  // String data = await asyncFunction();
+  // print('Data: $data');
+  print(await simulateAnOperation(11, 22));
 }
 
+// Simple function that returns a Future
 Future<String> asyncFunction() async {
   await Future.delayed(Duration(seconds: 2));
   return 'Hello, World!';
+}
+
+//
+Future<String> simulateAnOperation(int a, int b) async {
+  try {
+    print("Getting data .....");
+    await Future.delayed(Duration(seconds: 15));
+    int c = a + b;
+    return 'The result for sum operation is $c';
+  } on FormatException {
+    return "An error occured ! Giive right number";
+  }
 }
